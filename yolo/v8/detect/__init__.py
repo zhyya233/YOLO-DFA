56f7c22ac614411442a131f8075ece18a85af362
Hongyuan Zhang <212024085405009@stu.xhu.edu.cn>
2026-07-02 16:14:17 +0800

    Add files via upload

---
+++ yolo/v8/detect/__init__.py
@@ -0,0 +1,6 @@
+# Ultralytics YOLO 🚀, GPL-3.0 license
+
+from .predict import DetectionPredictor, predict
+# 这是新的、正确的代码
+from .train import DetectionTrainer
+from .val import DetectionValidator, val
